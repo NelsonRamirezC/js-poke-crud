1eb55836210db4a7e03fe778a9c3997127d37cb5
Nelson Ramirez <xhyskoax@gmail.com>
2024-03-26 20:51:34 -0300

    Se finaliza de manera temporal la función loadCardPokemon, agregango los tips y los stats del pokémon

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ $(function () {
         const regexValidacion = /^[a-záéíóúñ0-9\s.]+$/i;
 
         // let dataForm = new FormData(this);
-
         // let idOrName = dataForm.get("pokemon");
 
         let idOrName = $("#pokemon").val();
@@ -25,17 +24,33 @@ $(function () {
             url: urlBase,
             dataType: "json"
         }).done(function (response) {
-            console.log(response);
+
+            // console.log(response.stats);
+
             let pokemon = {
                 id: response.id,
                 name: response.name,
                 image: response.sprites.front_default,
-                expBase: response.base_experience
+                expBase: response.base_experience ?? 0,
+                types: response.types.map(element => {
+                    return element.type.name;
+                }),
+                stats: response.stats.map(element => {
+                    let stat = {
+                        name: element.stat.name,
+                        value: element.base_stat
+                    }
+                    return stat;
+                })
             };
+
+            // console.log(pokemon);
 
             loadCardPokemon(pokemon);
 
-        });
+        }).fail(function() {
+            alert("Error al procesar el pokémon, verifique el índice de pokémones de la guía oficial.");
+        })
     };
 
 
@@ -44,6 +59,24 @@ $(function () {
         $("#cardPokemon-name").text(pokemon.name);
         $("#cardPokemon-image").attr("src", pokemon.image);
         $("#cardPokemon-expBase").text(pokemon.expBase);
+
+        //recorrer types
+        let listItemTypes = "";
+        for (const type of pokemon.types) {
+            listItemTypes += `<li>${type}</li>`
+        };
+
+        $("#cardPokemon-typesList").html(listItemTypes);
+        
+
+    
+        //recorrer stats
+        let listStats = "";
+        for (const stat of pokemon.stats) {
+            listStats  += `<li>${stat.name}: ${stat.value}</li>`
+        };
+
+        $("#cardPokemon-statsList").html(listStats);
 
     }
 });
